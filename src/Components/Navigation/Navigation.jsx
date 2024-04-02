d29282e31f342ae69111e64c42d15d1febb68c37
BANDMardolI <artur.akhmadishin@mail.ru>
2024-04-02 23:38:31 +0400

    comm

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,27 +1,19 @@
-import React from 'react'
-import { useState } from 'react';
+import React from 'react';
 import "./Navigation.css";
 import NavigationLink from '../NavigationLink/NavigationLink';
 
 
 function Navigation(){
-  const [active, setActive] = useState(' active');
-  
-  function switchActivePage(){
-    setActive('')
-    console.log(active);
-  }
-
   return(
       <nav className='main_navigation'>
         <li className={'main_navigation__item'}>
-        <NavigationLink onClick={switchActivePage} itemClass={active} text={'Фильмы'} path={"/"}/>
+        <NavigationLink text={'Фильмы'} path={"/"}/>
         </li>
         <li className={'main_navigation__item'}>
-        <NavigationLink onClick={switchActivePage} text={'Телеканалы'} path={"/Channels"}/>
+        <NavigationLink text={'Телеканалы'} path={"/Channels"}/>
         </li>
       </nav>
   )
 }
 
-export default Navigation
+export default Navigation;
--- src/Components/NavigationLink/NavigationLink.jsx
+++ src/Components/NavigationLink/NavigationLink.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './NavigationLink.css'
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 
 
-function NavigationLink({text, path, itemClass}){
+function NavigationLink({text, path}){
     return(
-        <Link to ={path} href='#' className={"main_navigation__link" + itemClass}>
+        <NavLink to ={path} href='#' className={"main_navigation__link"}>
         {text}
-        </Link>
+        </NavLink>
     )
 
 }
